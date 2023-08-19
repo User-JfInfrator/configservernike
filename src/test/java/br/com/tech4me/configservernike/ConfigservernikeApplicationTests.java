0160dc350426455d9b386669b5470bff39cbbb7f
User-JfInfrator <jaozimvlogsamorim@gmail.com>
2023-08-18 21:28:08 -0300

    first commit

---
+++ src/test/java/br/com/tech4me/configservernike/ConfigservernikeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.configservernike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigservernikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
